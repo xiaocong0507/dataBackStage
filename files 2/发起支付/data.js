a6879022f1ff6e5379c362ea9e41e36263894dc7
Gary <389446599@qq.com>
2018-11-16 17:30:22 +0800

    v2.1
    v2.1

---
+++ files 2/发起支付/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="发起支付.html",d="generationDate",e=new Date(1535525501767.91),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="60578e181ff445ee9b968f8ebb16b037",q="type",r="Axure:Page",s="name",t="发起支付",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="d4c89bdedbb54f2d8223bfad13ef596e",Y="label",Z="friendlyType",ba="图片 ",bb="imageBox",bc="styleType",bd="visible",be=true,bf="75a91ee5b9d042cfa01b8d565fe289c0",bg="size",bh="width",bi=981,bj="height",bk=552,bl="imageOverrides",bm="images",bn="normal~",bo="images/发起支付/u989.jpg",bp="masters",bq="objectPaths",br="d4c89bdedbb54f2d8223bfad13ef596e",bs="scriptId",bt="u989";
+return _creator();
+})());
---
+++ files 2/运营后台/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(U,_(V,W,X,[_(V,Y,Z,g,ba,[_(bb,bc,V,bd,be,_(bf,n,b,bg,bh,bi),bj,bk)])])),bl,_(bm,[])),bn,_(),bo,_());}; 
+var b="url",c="运营后台.html",d="generationDate",e=new Date(1535525499783.26),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="b83712d980124493a73feea233cedecd",q="type",r="Axure:Page",s="name",t="运营后台",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="onLoad",V="description",W="页面载入时 ",X="cases",Y="Case 1",Z="isNewIfGroup",ba="actions",bb="action",bc="linkWindow",bd="在 当前窗口 打开 登录注册",be="target",bf="targetType",bg="登录注册.html",bh="includeVariables",bi=true,bj="linkType",bk="current",bl="diagram",bm="objects",bn="masters",bo="objectPaths";
+return _creator();
+})());
